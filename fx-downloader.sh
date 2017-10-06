282dcbb8d6abd286f26b7e8d807e0a9fa04ed0dd
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2017-10-06 22:28:10 +0200

    Added ability to download older versions
    Run the script using ./fx-downloader.sh [old version]
    
    [old version] = the amount of version, counted from the latest build (0) to go back.
    [old version] = 2 means download the build that came 2 builds BEFORE the latest one. (eg: latest = 10, [old version] = 2 would result in download of build #8)

--- fx-downloader.sh
+++ fx-downloader.sh
@@ -4,7 +4,12 @@
 # Description: Script that finds and downloads the latest version of an FX Server (from the fivem runtime website).
 # Author: http://github.com/tomgrobbe
 # Date: 06-Oct-2017
-# Version: 1.0
+# Version: 1.1
+# Usage: ./fx-downloader.sh [previous version number (default: 0 for latest build)]
+# Example: We want to download the latest version, latest build is build #400
+# ./fx-downloader.sh 0
+# Example: We want to download 2 versions BEFORE the latest build, latest build is #400 (we want build #398)
+# ./fx-downloader.sh 2
 
 # Storing the link containing all latest builds in a variable.
 link="https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/"
@@ -12,13 +17,18 @@ link="https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/"
 # Download the webpage form above and save it as index.html
 wget $link
 
+# Get the amount of versions to go back to from the argument passed while executing this script
+# use 0 for the latest version.
+version=$(($1 + 3))
+
+
 # Then using some fancy hacky methods, we read the index.html file,
 # take what we need (only the last 3 lines, strip leading and trailing characters
 # and we grep - (search for lines containing a -, which is only one line).
 # Now we end up with the name of the latest build and we use that to make the download
 # link for the latest build.
 # We save the output of the executed command in a new variable called latestBuild.
-latestBuild=$(tail -n 3 index.html | cut -c 10-54 | grep -)
+latestBuild=$(tail -n $version index.html | head -n 1 | cut -c 10-54)
 
 # We remove index.html because we don't need that anymore.
 rm index.html
@@ -27,3 +37,8 @@ rm index.html
 # We now end up with te latest version of fx.tar.xz
 wget $link$latestBuild"/fx.tar.xz"
 
+echo "#####################################################################"
+
+echo "  Downloaded version: $latestBuild"
+
+echo "#####################################################################"
